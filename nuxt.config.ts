833d67c60107e137adbda1a064489bad686f30b9
Dylan Tientcheu <dylantientcheu@gmail.com>
2021-12-18 13:49:53 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,26 @@
+import { defineNuxtConfig } from 'nuxt3'
+
+export default defineNuxtConfig({
+  meta: {
+    title: 'Vitesse Nuxt 3',
+  },
+  buildModules: [
+    '@vueuse/nuxt',
+    '@unocss/nuxt',
+    '@pinia/nuxt',
+  ],
+  unocss: {
+    uno: true,
+    attributify: true,
+    preflight: true,
+    icons: {
+      scale: 1.2,
+    },
+    shortcuts: [
+      ['btn', 'px-4 py-1 rounded inline-block bg-teal-600 text-white cursor-pointer hover:bg-teal-700 disabled:cursor-default disabled:bg-gray-600 disabled:opacity-50'],
+    ],
+  },
+  vite: {
+    logLevel: 'info',
+  },
+})

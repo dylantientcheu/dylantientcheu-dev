d3aceec1a511749938579bed6cce193c8a80b6a2
Dylan Tientcheu <dylantientcheu@gmail.com>
2021-12-28 19:39:58 +0100

    update header liniks

--- components/Header.vue
+++ components/Header.vue
@@ -8,8 +8,8 @@
       justify-center
       gap-3
     >
-      <a class="i-carbon-logo-github" href="https://github.com/antfu/vitesse-nuxt3" target="__blank" />
-      <a i-carbon:logo-linkedin href="https://github.com/antfu/vitesse-nuxt3" target="__blank" />
+      <a class="i-carbon-logo-github" href="https://github.com/blurdylan" target="__blank" />
+      <a i-carbon:logo-linkedin href="https://www.linkedin.com/in/dylan-tientcheu/" target="__blank" />
       <a i-carbon:notebook href="https://blog.dylantientcheu.dev" target="__blank" />
       <DarkToggle />
     </div>

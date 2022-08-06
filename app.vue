b7584c1a7384d53926f9b418f5db5f76d81fdf7b
Dylan Tientcheu <dylantientcheu@gmail.com>
2022-08-06 15:44:42 +0200

    feat: improve site meta

--- app.vue
+++ app.vue
@@ -1,3 +1,31 @@
+<script setup>
+useHead({
+  title: "Dylan Tientcheu",
+  meta: [
+    { charset: "utf-8" },
+    { name: "viewport", content: "width=device-width, initial-scale=1" },
+    {
+      hid: "description",
+      name: "description",
+      content: "Dylan Tientcheu | 10x Software Engineer",
+    },
+    { name: "og:url", content: "https://dylantientcheu.dev/" },
+    { name: "og:type", content: "website" },
+    { name: "og:title", content: "Dylan Tientcheu" },
+    { name: "og:description", content: "10x Software Engineer" },
+    { name: "og:image", content: "https://dylantientcheu.dev/og.png" },
+    { name: "twitter:card", content: "summary_large_image" },
+    { name: "twitter:site", content: "@dylantientcheu" },
+    { name: "twitter:creator", content: "@dylantientcheu" },
+    { name: "twitter:title", content: "Dylan Tientcheu" },
+    { name: "twitter:description", content: "10x Software Engineer" },
+    { name: "twitter:image", content: "https://dylantientcheu.dev/og.png" },
+  ],
+
+  link: [{ rel: "icon", type: "image/png", href: "/icon.png" }],
+});
+</script>
+
 <template>
   <div class="p-4">
     <Header />
@@ -7,25 +35,6 @@
   </div>
 </template>
 
-<script setup>
-useHead(
-  {
-		title: "Dylan Tientcheu",
-		meta: [
-			{ charset: "utf-8" },
-			{ name: "viewport", content: "width=device-width, initial-scale=1" },
-			{
-				hid: "description",
-				name: "description",
-				content:
-					"Dylan Tientcheu | 10x Software Engineer",
-			},
-		],
-		link: [{ rel: "icon", type: "image/png", href: "/icon.png" }],
-	},
-)
-</script>
-
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Jost:wght@300;400;600;700;900&display=swap");
 @import url("https://fonts.googleapis.com/css2?family=Square+Peg");
@@ -33,21 +42,21 @@ useHead(
 html,
 body,
 #__nuxt {
-	height: 100vh;
-	margin: 0;
-	padding: 0;
-	font-family: "Jost", sans-serif;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+  font-family: "Jost", sans-serif;
   font-size: 1.1rem;
 }
 
 html.dark {
-	background: #121212;
-	color: #d3d3d3;
+  background: #121212;
+  color: #d3d3d3;
 }
 
 a {
-	font-weight: 600;
-	text-decoration: underline;
+  font-weight: 600;
+  text-decoration: underline;
 }
 
 .cursivo {
@@ -58,5 +67,4 @@ a.cursivo {
   font-family: "Square Peg", cursive;
   font-size: 1.5rem;
 }
-
 </style>

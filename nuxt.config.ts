3cbe47627554bb67dcfb722809e5b373f16352f6
Dylan Tientcheu <dylantientcheu@gmail.com>
2023-01-07 13:27:42 +0100

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,23 +1,20 @@
-import { defineNuxtConfig } from 'nuxt'
-
 export default defineNuxtConfig({
-	target: 'static',
-	modules: ["@vueuse/nuxt", "@unocss/nuxt", "@nuxtjs/color-mode"],
-	unocss: {
-		preflight: true,
-	},
-	experimental: {
-		reactivityTransform: true,
-		viteNode: false,
-	},
-	colorMode: {
-		classSuffix: "",
-	},
-	hooks: {
-		"vite:extendConfig": function (config: any, { isServer }: any) {
-			if (isServer) {
-				config.build.rollupOptions.output.inlineDynamicImports = true;
-			}
-		},
-	},
-});
+  target: 'static',
+  modules: ['@vueuse/nuxt', '@unocss/nuxt', '@nuxtjs/color-mode'],
+  unocss: {
+    preflight: true,
+  },
+  experimental: {
+    reactivityTransform: true,
+    viteNode: false,
+  },
+  colorMode: {
+    classSuffix: '',
+  },
+  hooks: {
+    'vite:extendConfig': function (config: any, { isServer }: any) {
+      if (isServer)
+        config.build.rollupOptions.output.inlineDynamicImports = true
+    },
+  },
+})
